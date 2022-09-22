20b5d1af63b840a9c87ca5f1440dcf4131fcf442
DylanDev <dylanm.ravel@gmail.com>
2022-09-22 09:10:56 -0600

    test

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+import $ from 'jquery'
+import 'popper.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
+
 import Swup from 'swup';
 const swup = new Swup();
+
+
+$(document).ready(()=>{
+    $('body').html('Hello World, if you see this message Parcel Bundler and Github Hosting are working nicely!');
+})
