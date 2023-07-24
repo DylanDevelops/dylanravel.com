0a2bf203421700913c43263e32ffb5cd7dc1fdfb
Dylan Ravel <dylanm.ravel@gmail.com>
2023-07-24 00:15:19 -0600

    working hard at the redesign

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-import $ from 'jquery'
-import 'popper.js'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap'
-
-import Swup from 'swup';
-const swup = new Swup();
-
-
-$(document).ready(()=>{
-    $('body').html('Hello World, if you see this message Parcel Bundler and Github Hosting are working nicely!');
-})
+function clickNavPage(pageUrl) {
+    window.location.href = pageUrl;
+}
