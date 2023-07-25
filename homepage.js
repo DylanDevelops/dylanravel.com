d48fd7c3c68067ae8ddd97e309a569a58ba55c76
Dylan Ravel <dylanm.ravel@gmail.com>
2023-07-24 22:58:31 -0600

    more easter eggs

--- homepage.js
+++ homepage.js
@@ -13,14 +13,17 @@ document.addEventListener('DOMContentLoaded', function() {
     function toggleRandomEffect() {
         if (!profilePic.classList.contains('spinning') && !profilePic.classList.contains('zooming') && !profilePic.classList.contains('bouncing')) {
             // Generate a random number (0, 1, or 2) to determine which animation to play
-            const randomEffect = Math.floor(Math.random() * 3);
+            const randomEffect = Math.floor(Math.random() * 4);
             
             if (randomEffect === 0) {
                 profilePic.classList.add('spinning');
             } else if (randomEffect === 1) {
                 profilePic.classList.add('zooming');
-            } else {
+            } else if(randomEffect === 2) {
                 profilePic.classList.add('bouncing');
+            } else {
+                // take me to a link and target _blank
+                window.open('./Assets/Images/teehee.jpg', '_blank');
             }
             
             // Add animationend event listener to reset the animation state
