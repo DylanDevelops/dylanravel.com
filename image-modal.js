905e21c0b14e1fa481839d02d6e059e31573f4ae
Dylan Ravel <dylanm.ravel@gmail.com>
2023-08-17 13:43:27 -0600

    got a start on this code

---
+++ image-modal.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // get the modal
+    const modal = document.getElementById("modal-popout");
+
+    // get the image and insert it inside the modal and use its alt text as caption
+    const img = document.getElementById
+});
