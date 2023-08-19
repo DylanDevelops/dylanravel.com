171c528e2cd978482feef2f2e94efc520ba93668
Dylan Ravel <dylanm.ravel@gmail.com>
2023-08-19 07:31:52 -0600

    Got the system working and its done

--- image-modal.js
+++ image-modal.js
@@ -1,7 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
     // get the modal
     const modal = document.getElementById("modal-popout");
+    const closeButton = document.getElementById("modal-close-button");
+    const projectImages = document.querySelectorAll(".project-image");
+    const modalImage = document.getElementById('modal-image');
+    const caption = document.getElementById('caption');
 
-    // get the image and insert it inside the modal and use its alt text as caption
-    const img = document.getElementById
+    projectImages.forEach(image => {
+        image.addEventListener('click', clickModalPopout)
+    });
+
+    function clickModalPopout(img) {
+        // get the clicked image
+        const clickedImage = img.target;
+
+        // grab data on the image
+        modalImage.src = clickedImage.src;
+        caption.textContent = clickedImage.alt;
+
+        modal.style.display = "block";
+    }
+    
+    // modal close button listener
+    closeButton.addEventListener('click', () => {
+        modal.style.display = "none";
+    });
 });
