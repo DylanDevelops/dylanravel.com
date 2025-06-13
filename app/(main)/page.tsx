0ebab1613da307c82d9a50e16b355d0d6c7b65ae
Dylan Ravel <dylanm.ravel@gmail.com>
2025-06-12 22:43:28 -0600

    More work

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
+
+import "./_components/frosted-glass.css";
+
 const MainPage = () => {
   return (
-    <div className="w-screen min-h-screen flex flex-col p-0 m-0 bg-[#202020] bg-[url('/backgrounds/shapes.png')] bg-repeat bg-[length:600px_600px]">
-      <main className="flex flex-col flex-1 justify-center items-center space-y-2 p-10">
-        <div className="flex flex-col w-full bg-white">
-          <p>Test</p>
+    <div className="relative w-screen min-h-screen flex flex-col p-0 m-0 bg-[#202020] bg-[url('/backgrounds/shapes.png')] bg-repeat bg-[length:600px_600px]">
+      <div className="absolute inset-0 bg-[#202020]/80 pointer-events-none z-0" />
+      <main className="relative flex flex-col flex-1 justify-center items-center space-y-2 p-10 z-10">
+        <div className="flex flex-row items-center w-full h-64 frosted-glass-card p-10">
+          <div>
+            <Image />
+          </div>
         </div>
       </main>
     </div>
