243a8bfbc41c19ef543a831ebac523fd085c3617
Dylan Ravel <dylanm.ravel@gmail.com>
2025-06-17 17:49:07 -0600

    Finished hello and about section for now

--- app/(main)/_components/sections/hello.tsx
+++ app/(main)/_components/sections/hello.tsx
@@ -1,15 +1,65 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+import "./_components/hello.css";
+
+import { ProfileInformation } from "@/lib/branding";
+
 const Hello = () => {
+  const headerText = `Hey, I'm ${ProfileInformation.FirstName}!`.split("");
+  const wavingEmoji = "👋";
+
   return (
-    <section id="hello" className="flex flex-col gap-2 scroll-mt-10">
-      <h2 className="text-white font-bold text-2xl">Hey, I'm Dylan!</h2>
-      <p className="text-white/80">
-        I am a game developer, programmer, and student passionate about building scalable, creative, and innovative
-        applications.
-      </p>
-      <p className="text-white/80">
-        With intimate knowledge of full-stack software development, project management, and game design, I build systems
-        that allow users to see the world in a new way.
-      </p>
+    <section id="hello" className="flex flex-col gap-4 scroll-mt-10">
+      <h2 className="text-white font-bold text-4xl">
+        {headerText.map((char, i) => (
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 0.25,
+              delay: i / 10,
+            }}
+            key={i}
+          >
+            {char}
+          </motion.span>
+        ))}{" "}
+        <motion.span
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            rotate: [0, 15, -15, 15, 0],
+          }}
+          transition={{
+            opacity: {
+              duration: 0.25,
+              delay: headerText.length / 10,
+            },
+            rotate: {
+              duration: 1,
+              repeat: 2,
+              repeatType: "mirror",
+            },
+          }}
+          key="emoji"
+          style={{ display: "inline-block" }}
+        >
+          {wavingEmoji}
+        </motion.span>
+      </h2>
+      <motion.p
+        className="text-white/80 text-lg font-bold"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.5,
+          delay: headerText.length * 0.1 + 1,
+        }}
+      >
+        Game Developer • Programmer • Research Assistant • Student
+      </motion.p>
     </section>
   );
 };
