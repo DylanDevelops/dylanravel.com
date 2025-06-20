7a67bcc5c721828a4d0f5e11c16d9d96ad1462e2
Dylan Ravel <dylanm.ravel@gmail.com>
2025-06-20 00:08:22 -0600

    Reduced file size of images

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -11,7 +11,7 @@ import Skills from "./_components/sections/skills";
 
 const MainPage = () => {
   return (
-    <div className="relative w-screen min-h-screen flex flex-col p-0 m-0 bg-[#202020] bg-[url('/backgrounds/shapes.png')] bg-repeat bg-[length:600px_600px]">
+    <div className="relative w-screen min-h-screen flex flex-col p-0 m-0 bg-[#202020] bg-[url('/backgrounds/shapes.webp')] bg-repeat bg-[length:600px_600px]">
       <div className="absolute inset-0 bg-[#202020]/80 pointer-events-none z-0" />
       <main className="md:relative flex flex-col gap-2 flex-1 justify-center items-center space-y-2 p-4 z-10 mt-20">
         <Navbar />
