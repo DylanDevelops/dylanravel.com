f02dd7f1e72b3bdad8c05966d218bded3093583f
Dylan Ravel <dylanm.ravel@gmail.com>
2026-05-13 09:59:38 -0700

    Add a bunch of redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,52 @@
 import type { NextConfig } from "next";
 
+import { GitHubURL, InstagramURL, ItchIoURL, KoFiURL, LinkedInURL, ORCIDUrl } from "./lib/links";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/contact",
+        destination: "/#contact",
+        permanent: true,
+      },
+      {
+        source: "/about",
+        destination: "/#about",
+        permanent: true,
+      },
+      {
+        source: "/github",
+        destination: GitHubURL,
+        permanent: true,
+      },
+      {
+        source: "/linkedin",
+        destination: LinkedInURL,
+        permanent: true,
+      },
+      {
+        source: "/instagram",
+        destination: InstagramURL,
+        permanent: true,
+      },
+      {
+        source: "/itch",
+        destination: ItchIoURL,
+        permanent: true,
+      },
+      {
+        source: "/orcid",
+        destination: ORCIDUrl,
+        permanent: true,
+      },
+      {
+        source: "/kofi",
+        destination: KoFiURL,
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
